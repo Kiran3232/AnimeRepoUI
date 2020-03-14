15cc390d611315cfc1b86dcd189c94ea456719a2
Kiran3232 <37551543+Kiran3232@users.noreply.github.com>
2020-03-14 22:57:54 +0530

    server.js | Fix for dist path

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/animerepo'));
+app.use(express.static(__dirname + '/dist/anime-repo'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/animerepo/index.html'));
+res.sendFile(path.join(__dirname+'/dist/anime-repo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
