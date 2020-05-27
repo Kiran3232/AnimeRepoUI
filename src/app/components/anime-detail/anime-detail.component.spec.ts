e4c897de8002c3476fd249b38f7fc82e0aa9f888
Kiran3232 <37551543+Kiran3232@users.noreply.github.com>
2020-05-28 00:26:52 +0530

    Removed Test Cases

--- src/app/components/anime-detail/anime-detail.component.spec.ts
+++ src/app/components/anime-detail/anime-detail.component.spec.ts
@@ -19,7 +19,7 @@ describe('AnimeDetailComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
--- src/app/components/edit-anime/edit-anime.component.spec.ts
+++ src/app/components/edit-anime/edit-anime.component.spec.ts
@@ -19,7 +19,7 @@ describe('EditAnimeComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
