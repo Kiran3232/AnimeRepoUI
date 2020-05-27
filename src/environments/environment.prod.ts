31b6b2a83649543c9cda509bafdecd192eee9a7c
Kiran3232 <37551543+Kiran3232@users.noreply.github.com>
2020-05-27 15:12:45 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBSBcDjstCIAJ3j8wiSHSvv5FMpLJLReh8",
+    authDomain: "animerepo-f3165.firebaseapp.com",
+    databaseURL: "https://animerepo-f3165.firebaseio.com",
+    projectId: "animerepo-f3165",
+    storageBucket: "animerepo-f3165.appspot.com",
+    messagingSenderId: "1017695525041",
+    appId: "1:1017695525041:web:987ed0ca6c7defe75f2a90",
+    measurementId: "G-8Y4CD9YWJW"
+  }
 };
